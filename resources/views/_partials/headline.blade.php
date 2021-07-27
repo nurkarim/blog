67e2d0354dbda67d11933dbf34ea901371c76953
Nur Karim <nurkarim772@gmail.com>
2021-07-27 18:48:07 +0600

    headline modify

--- resources/views/_partials/headline.blade.php
+++ resources/views/_partials/headline.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-body-color ml-15 pr-15 mb-10 mt-10">
         <div class="row no-gutters d-flex align-items-center">
             <div class="col-lg-2 col-md-3 col-sm-4 col-5">
-                <div class="topic-box">Top Stories</div>
+                <div class="topic-box">Latest</div>
             </div>
             <div class="col-lg-10 col-md-9 col-sm-8 col-7">
                 <div class="feeding-text-light2">
