71da57e92d87417e90b900f699cdc71acd37d5fa
Nur Karim <nurkarim772@gmail.com>
2021-07-28 00:17:54 +0600

    headline modify

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,12 +33,16 @@
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','G-WW5LSWZDX1');</script>
+        })(window,document,'script','dataLayer','GTM-PDDHLCP');</script>
     <!-- End Google Tag Manager -->
+
 </head>
 
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDDHLCP"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <div id="wrapper">
     <!-- Header Area Start Here -->
