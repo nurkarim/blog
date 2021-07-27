ddc715257a009c2e63f7aeed675fe132c7f19f50
Nur Karim <nurkarim772@gmail.com>
2021-07-27 19:13:43 +0600

    headline modify

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'FrontController@index')->middleware('pageSpeed');
 Route::get('/category/{slug}', 'FrontController@category')->middleware('pageSpeed');
-Route::get('/story/{slug}', 'FrontController@details')->middleware('pageSpeed');
+Route::get('/story/{slug}', 'FrontController@details');
 
 
 Auth::routes();
