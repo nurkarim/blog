b849069c902d243d1baeb8e78365af422f44e803
Nur Karim <nurkarim772@gmail.com>
2021-08-01 12:42:59 +0600

    css and javascript modify

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -7,25 +7,25 @@
 <script src="{{ url('public') }}/js/popper.js" type="text/javascript"></script>
 <!-- Bootstrap js -->
 <script src="{{ url('public') }}/js/bootstrap.min.js" type="text/javascript"></script>
-<!-- WOW JS -->
-<script src="{{ url('public') }}/js/wow.min.js"></script>
-<!-- Owl Cauosel JS -->
-<script src="{{ url('public') }}/vendor/OwlCarousel/owl.carousel.min.js" type="text/javascript"></script>
+{{--<!-- WOW JS -->--}}
+{{--<script src="{{ url('public') }}/js/wow.min.js"></script>--}}
+{{--<!-- Owl Cauosel JS -->--}}
+{{--<script src="{{ url('public') }}/vendor/OwlCarousel/owl.carousel.min.js" type="text/javascript"></script>--}}
 <!-- Meanmenu Js -->
 <script src="{{ url('public') }}/js/jquery.meanmenu.min.js" type="text/javascript"></script>
 <!-- Srollup js -->
 <script src="{{ url('public') }}/js/jquery.scrollUp.min.js" type="text/javascript"></script>
 <!-- jquery.counterup js -->
-<script src="{{ url('public') }}/js/jquery.counterup.min.js"></script>
-<script src="{{ url('public') }}/js/waypoints.min.js"></script>
+{{--<script src="{{ url('public') }}/js/jquery.counterup.min.js"></script>--}}
+{{--<script src="{{ url('public') }}/js/waypoints.min.js"></script>--}}
 <!-- Nivo slider js -->
 <script src="{{ url('public') }}/vendor/slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
 <script src="{{ url('public') }}/vendor/slider/home.js" type="text/javascript"></script>
-<!-- Isotope js -->
-<script src="{{ url('public') }}/js/isotope.pkgd.min.js" type="text/javascript"></script>
-<!-- Magnific Popup -->
-<script src="{{ url('public') }}/js/jquery.magnific-popup.min.js"></script>
-<!-- Ticker Js -->
+{{--<!-- Isotope js -->--}}
+{{--<script src="{{ url('public') }}/js/isotope.pkgd.min.js" type="text/javascript"></script>--}}
+{{--<!-- Magnific Popup -->--}}
+{{--<script src="{{ url('public') }}/js/jquery.magnific-popup.min.js"></script>--}}
+{{--<!-- Ticker Js -->--}}
 <script src="{{ url('public') }}/js/ticker.js" type="text/javascript"></script>
 <!-- Custom Js -->
 <script src="{{ url('public') }}/js/main.js" type="text/javascript"></script>
